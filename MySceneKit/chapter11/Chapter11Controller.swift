38a76fba3287bc0582c357be911a30cc5c6715a6
Qingcan <q_li@fullspeed.co.jp>
2017-10-11 09:27:04 +0900

    add beard

--- MySceneKit/chapter11/Chapter11Controller.swift
+++ MySceneKit/chapter11/Chapter11Controller.swift
@@ -23,10 +23,15 @@ class Chapter11Controller: UIViewController {
     lazy var trackEffect = AiyaTrackEffect()
     var trackInput: AiyaTrackInput!
     
-    lazy var scnView = SCNView()
-    lazy var scnNode = SCNNode()
+    lazy var headView = SCNView()
+    lazy var headNode = SCNNode()
     
-    var stickerOriginalSize: CGSize?
+    lazy var beardView = SCNView()
+    lazy var beardNode = SCNNode()
+    
+    var headSize: CGSize?
+    var beardSize: CGSize?
+    
     var frames = [UIImage]()
     var animationTime : Float = 0
     
@@ -35,7 +40,8 @@ class Chapter11Controller: UIViewController {
 
         setupCamera()
         loadGif()
-        setupSCNView()
+        setupHeaderView()
+        setupBeardView()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -70,24 +76,24 @@ extension Chapter11Controller {
         camera.addTarget(preview)
     }
     
-    fileprivate func setupSCNView() {
-        scnView.isHidden = true
-        scnView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
-        scnView.scene = SCNScene()
-        scnView.backgroundColor = UIColor.clear
-        view.addSubview(scnView)
+    fileprivate func setupHeaderView() {
+        headView.isHidden = true
+        headView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        headView.scene = SCNScene()
+        headView.backgroundColor = UIColor.clear
+        view.addSubview(headView)
         
-        let scnPlane = SCNPlane(width: scnView.frame.width, height: scnView.frame.height)
-        scnPlane.firstMaterial?.diffuse.contents = frames[4]
-        scnNode.geometry = scnPlane
-        scnNode.position = SCNVector3Make(0, 0, 0)
-        scnView.scene?.rootNode.addChildNode(scnNode)
+        let scnPlane = SCNPlane(width: headView.frame.width, height: headView.frame.height)
+        scnPlane.firstMaterial?.diffuse.contents = frames[0]
+        headNode.geometry = scnPlane
+        headNode.position = SCNVector3Make(0, 0, 0)
+        headView.scene?.rootNode.addChildNode(headNode)
         
         let time = TimeInterval(animationTime / Float(frames.count))
         var count = 0
         let action = SCNAction.run { [weak self] node in
             guard let `self` = self else {return}
-            self.scnNode.geometry?.firstMaterial?.diffuse.contents = self.frames[count]
+            self.headNode.geometry?.firstMaterial?.diffuse.contents = self.frames[count]
             if count < self.frames.count - 1 {
                 count += 1
             } else {
@@ -97,9 +103,24 @@ extension Chapter11Controller {
         let waitAction = SCNAction.wait(duration: time)
         let actions = SCNAction.sequence([action, waitAction])
         let repeatActions = SCNAction.repeatForever(actions)
-        scnNode.runAction(repeatActions)
+        headNode.runAction(repeatActions)
+    }
+    
+    fileprivate func setupBeardView() {
+        beardView.isHidden = true
+        beardView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        beardView.scene = SCNScene()
+        beardView.backgroundColor = UIColor.clear
+        view.addSubview(beardView)
         
-        scnView.allowsCameraControl = true
+        let beardImage = UIImage(named: "huxu.png")
+        beardSize = beardImage?.size
+        
+        let scnPlane = SCNPlane(width: beardView.frame.width, height: beardView.frame.height)
+        scnPlane.firstMaterial?.diffuse.contents = beardImage
+        beardNode.geometry = scnPlane
+        beardNode.position = SCNVector3Make(0, 0, 0)
+        beardView.scene?.rootNode.addChildNode(beardNode)
     }
     
     fileprivate func toScreenX(_ detectX: Float) -> CGFloat {
@@ -128,7 +149,7 @@ extension Chapter11Controller {
                 
                 let image = UIImage(cgImage: cgImage)
                 if i == 0 {
-                    stickerOriginalSize = image.size
+                    headSize = image.size
                 }
                 frames.append(image)
             }
@@ -145,7 +166,8 @@ extension Chapter11Controller: GPUImageVideoCameraDelegate {
     func willOutputSampleBuffer(_ sampleBuffer: CMSampleBuffer!) {
         if !trackEffect.trackSuccess() {
             DispatchQueue.main.async {
-                self.scnView.isHidden = true
+                self.headView.isHidden = true
+                self.beardView.isHidden = true
             }
             return
         }
@@ -156,21 +178,38 @@ extension Chapter11Controller: GPUImageVideoCameraDelegate {
         let rotateY = CGFloat(faceData.faceRotation.1)
         let rotateZ = CGFloat(faceData.faceRotation.2)
 
-        let centerX = toScreenX(faceData.featurePoints2D.68.0)
-        let centerY = toScreenY(faceData.featurePoints2D.68.1)
-        let width = toScreenX(faceData.featurePoints2D.24.0 - faceData.featurePoints2D.19.0)
-        var height = width
-        if let stickerOriginalSize = stickerOriginalSize {
-            height = width * stickerOriginalSize.height / stickerOriginalSize.width
+        // head.
+        let headCenterX = toScreenX(faceData.featurePoints2D.68.0)
+        let headCenterY = toScreenY(faceData.featurePoints2D.68.1)
+        let headWidth = toScreenX(faceData.featurePoints2D.24.0 - faceData.featurePoints2D.19.0)
+        var headHeight = headWidth
+        if let headSize = headSize {
+            headHeight = headWidth * headSize.height / headSize.width
+        }
+        
+        // beard.
+        let beardCenterX = toScreenX(faceData.featurePoints2D.33.0)
+        let beardCenterY = toScreenY(faceData.featurePoints2D.33.1 - (faceData.featurePoints2D.33.1 - faceData.featurePoints2D.51.1) / 2)
+        let beardWidth = toScreenX(faceData.featurePoints2D.15.0 - faceData.featurePoints2D.1.0) * 2 / 3
+        var beardHeight = beardWidth
+        if let beardSize = beardSize {
+            beardHeight = beardWidth * beardSize.height / beardSize.width
         }
 
         DispatchQueue.main.async {
-            self.scnView.frame = CGRect(x: 0, y: 0, width: width, height: height)
-            self.scnView.center = CGPoint(x: centerX, y: centerY)
-            self.scnView.isHidden = false
+            // head
+            self.headView.frame = CGRect(x: 0, y: 0, width: headWidth, height: headHeight)
+            self.headView.center = CGPoint(x: headCenterX, y: headCenterY)
+            self.headView.isHidden = false
+            
+            // beard.
+            self.beardView.frame = CGRect(x: 0, y: 0, width: beardWidth, height: beardHeight)
+            self.beardView.center = CGPoint(x: beardCenterX, y: beardCenterY)
+            self.beardView.isHidden = false
         }
 
         let rotation = SCNAction.rotateTo(x: rotateX, y: -rotateY, z: rotateZ, duration: 0)
-        scnNode.runAction(rotation)
+        headNode.runAction(rotation)
+        beardNode.runAction(rotation)
     }
 }
